f953af01bfb36e6c67451d16fcf85cf43c435bb3
mcgear <michael.gearhardt@gmail.com>
2023-08-21 23:15:44 -0600

    Correct export location

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,7 +1,6 @@
 // export * from "../../../fathym-deno/atomic/mod.ts";
 export * from "https://deno.land/x/fathym_atomic@v0.0.45-integration/mod.ts";
-
-export * from "../../../fathym-deno/atomic-social/mod.ts";
-// export * from "https://deno.land/x/fathym_atomic_social@v0.0.5-integration/mod.ts";
+// export * from "../../../fathym-deno/atomic-social/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_social@v0.0.7-integration/mod.ts";
 export * from "https://esm.sh/*@preact/signals@1.1.5";
 export * from "https://esm.sh/preact@10.16.0";
