10b3f8d65251669c208fef3d622335a06d9f8928
mcgear <michael.gearhardt@gmail.com>
2023-10-11 10:12:38 -0600

    Upgrades

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,6 +1,5 @@
 // export * from "../../../fathym-deno/atomic/mod.ts";
-export * from "https://deno.land/x/fathym_atomic@v0.0.45-integration/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.90/mod.ts";
 // export * from "../../../fathym-deno/atomic-social/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_social@v0.0.7-integration/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_social@v0.0.44/mod.ts";
 export * from "https://esm.sh/*@preact/signals@1.1.5";
-export * from "https://esm.sh/preact@10.16.0";
--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -1,2 +1,10 @@
-export { assert, assertEquals } from "$std/testing/asserts.ts";
-export { afterEach, beforeEach, describe, it } from "$std/testing/bdd.ts";
+export {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.203.0/testing/asserts.ts";
+export {
+  afterEach,
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.203.0/testing/bdd.ts";
